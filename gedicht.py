dbaa273ac10e851570663f419cb7ee8d4ec8ae12
Jayjay110 <jaydendodo@hotmail.com>
2020-09-30 11:43:42 +0200

    gedicht

---
+++ gedicht.py
@@ -0,0 +1,10 @@
+vers1="Mijn naam is Jayden Held"
+vers2="Ben jij niet blij dat ik jouw dit heb verteld?"
+vers3="Dan zou ik wel willen weten"
+vers4="wat jouw naam is, ook al ga ik het vergeten"
+vers5="genoeg met de namen dan voor nu"
+vers6="ik heb op het moment een deja vu"
+vers7="had ik mezelf niet al eerder een keer voorgesteld"
+vers8="nou maakt niet uit, dat je me kent is wat telt"
+
+print(vers1 , vers2 , vers3 , vers4 , vers5, vers6 , vers7 , vers8)
